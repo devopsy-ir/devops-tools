8c7c36abdcd1e97a4fe8a7eb98dc617cf355a698
محسن عباسی <developer961@gmail.com>
2022-01-05 08:12:46 +0330

    Add prmetheus admin pass to Grafana datasource provisioning yaml file

---
+++ monitor/run.sh
@@ -0,0 +1,5 @@
+password=$(fgrep PROM_ADMIN_PASS .env)
+password=$(cut -d "=" -f2- <<< "$password")
+hashedpass=$(echo "pip install bcrypt --disable-pip-version-check && python gen-pass.py $password" | docker run -i --rm --name my-running-script -v "$PWD/files/prometheus/gen-pass.py":/usr/src/myapp/gen-pass.py -w /usr/src/myapp python:3 /bin/bash)
+hashedpass=${hashedpass##*$'\n'}
+echo -en "basic_auth_users:\n  admin: $hashedpass" > files/prometheus/web.yml

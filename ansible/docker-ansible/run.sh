f475c1f697d30524aa2da4fac4f266c3fc1e83d9
Mohsen Abbasi <developer961@gmail.com>
2022-01-10 03:01:08 -0500

    Ansible based on distribution - Debian is correct

--- ansible/docker-ansible/run.sh
+++ ansible/docker-ansible/run.sh
@@ -3,4 +3,4 @@ apt update
 apt install -y curl
 compose_version=$(curl -Ls -o /dev/null -w %{url_effective}  https://github.com/docker/compose/releases/latest  | grep -o '[^/]*$')
 sed -i -E "s#compose_version:(.*)#compose_version: ${compose_version}#g" vars/docker.yml
-ansible-playbook -i hosts -u ubuntu docker-playbook.yml
+ansible-playbook -i hosts -u root tasks/main.yml
